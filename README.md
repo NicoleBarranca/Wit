eef4fa363141900ad39c4437c5369a80d2b5ccfc
Nicole Barranca <nikkibarranca@gmail.com>
2022-03-08 11:27:48 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Wit
+
+
+  <p align="center">
+  <a href="https://nicolebarranca.github.io/Wit/" rel="noopener">
+ <img width=200px height=200px src="./assets/images/Wit-Logo.jpg" alt="Project logo"></a>
+</p>
+
+<h1 align="center">Wit</h1>
+
+
+  ## Description
+  This website is a one stop shop for all things laughter. 
+  
+---
+
+  #### Features of Wit:
+  ```
+  -> Joke of the day
+  -> Excuses for all occasions 
+  -> Random and useless facts
+
+  ```
+  
+  ## Built With
+  - JavaScript
+  - HTML
+  - CSS
+
+
+## Website
+
+Check out [Wit](https://nicolebarranca.github.io/Wit/)!
+
+ <img width=350px height=209px src="./assets/images/Wit-Home.png" alt="Wit homepage"></a>
+
+
+
+
+## Contribution
+Made with ❤️ by Nicole Barranca
+
+
+## Questions
+
+Feel free to contact me directly at nikkibarranca@gmail.com if you have any questions about the repo. 
+
+You can find more of my work at [Nicole Barranca](https://github.com/NicoleBarranca).
--- assets/js/script.js
+++ assets/js/script.js
@@ -19,37 +19,51 @@ closeBtn.addEventListener('click', closeModal);
 //Listen for outside click
 window.addEventListener('click', clickOutside);
 // funtion to open modal
-function openModal(){
+function openModal() {
     modal.style.display = 'block';
 }
 
 // function to close modal
-function closeModal(){
+function closeModal() {
     modal.style.display = 'none';
 }
 
 // function to close modal if outside it clicked
-function clickOutside(event){
- if (event.target === modal){
-    modal.style.display = 'none';
- }
+function clickOutside(event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
 }
 
-var jokeOfDay = document.querySelector("#jokes") ;
+var jokeOfDay = document.querySelector("#jokes");
 var jokeResults = jokeOfDay.value;
 
 function get_joke_of_the_day() {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-	 if (this.readyState == 4 && this.status == 200) {
-	     // Access the result here
-	     alert(this.responseText);
-	 }
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // Access the result here
+            alert(this.responseText);
+        }
+// console.log(success.contents.categories[2])
+//         // var div = document.createElement("div");
+    
+//         // // div.textContent = jokeResults;
+//         // p.src = success.contents.categories[2]
+
+//         // jokeOfDay.appendChild(div);
+
     };
     xhttp.open("GET", "https://api.jokes.one/jod/categories", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.setRequestHeader("X-JokesOne-Api-Secret", "https://api.jokes.one/jod/categories");
+    xhttp.setRequestHeader("categories", "application/json");
+    // xhttp.setRequestHeader("X-JokesOne-Api-Secret", "https://api.jokes.one/jod/categories");
     xhttp.send();
 }
 
-get_joke_of_the_day()
+
+
+get_joke_of_the_day();
+
+// {"success":{"total":4},"contents":{"categories":[{"name":"jod","description":"Joke of the day ","language":"
+// en","background":""},{"name":"animal","description":"Animal Joke of the day ","language":"en","background":""},{"name":"blonde","description":"Blonde joke of the day!","language":"en","background":""},{"name":"knock-knock","description":"Knock Knock joke of the day!","language":"en","background":""}],"copyright":"2019-20 https:\/\/jokes.one"}}
+
